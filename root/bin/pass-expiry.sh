7c1b2ae109071da36048a8857711de8429d2896e
Emory Merryman <>
2018-01-22 01:15:11 +0000

    show author date

--- root/bin/pass-expiry.sh
+++ root/bin/pass-expiry.sh
@@ -3,9 +3,10 @@
 NOW=$(date +%s) &&
     pass git ls-tree -r --name-only HEAD | grep ".gpg\$" | while read FILE
     do
-        PASSWORD_AGE=$((${NOW}-$(pass git log -1 --format=%at -- ${FILE}))) &&
+        PASSWORD_AUTHOR_DATE=$(pass git log -1 --format=%at -- ${FILE}) &&
+            PASSWORD_AGE=$((${NOW}-${PASSWORD_AUTHOR_DATE})) &&
             PASSWORD_AGE_LENGTH=${#PASSWORD_LENGTH} &&
             PASSWORD_LENGTH=$(pass show ${FILE%.*} | wc --bytes) &&
             PASSWORD_LENGTH_LENGTH=${#PASSWORD_LENGTH} &&
-            echo ${PASSWORD_LENGTH_LENGTH} ${PASSWORD_AGE_LENGTH} ${PASSWORD_LENGTH} $(date --date @${PASSWORD_AGE} +%Y-%b-%d) ${FILE%.*}
+            echo ${PASSWORD_LENGTH_LENGTH} ${PASSWORD_AGE_LENGTH} ${PASSWORD_LENGTH} $(date --date @${PASSWORD_AUTHOR_DATE} +%Y-%b-%d) ${FILE%.*}
     done | sort -nk 2 | sort -rnk 1
